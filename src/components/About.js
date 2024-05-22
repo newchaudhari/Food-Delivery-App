a20abb4b0207a96e91fad83512bc4d9448b06407
newchaudhari <chaudharinavin1312@gmail.com>
2024-05-22 08:46:07 +0530

    api call in class based component

--- src/components/About.js
+++ src/components/About.js
@@ -2,25 +2,12 @@ import UserClass from "./UserClass";
 import React from "react";
 
 class About extends React.Component {
-
-  constructor(props){
-    super(props);
-    console.log("Parent constructor")
-  }
-
-  componentDidMount(){
-    console.log("Parent mount");
-  }
-
   render() {
-    console.log("Parent Render");
     return (
       <div className="about-body">
         <h1>About</h1>
         <h2>Namaste React Course</h2>
-        <UserClass name={"First"} location={"Pune"} />
-        <UserClass name={"Second"} location={"Bhusawal"} />
-        {console.log("Parent render 2")}
+        <UserClass name={"First"} location={"Pune"} contact={"@newchaudhari"} />
       </div>
     );
   }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,37 +5,37 @@ class UserClass extends React.Component {
     super(props);
 
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "dummy",
+        location: "default",
+        login: "@dummy ",
+      },
     };
-
-    console.log(this.props.name + " Children Constructor");
   }
 
-  componentDidMount() {
-    console.log(this.props.name + " Children Mount");
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/newchaudhari");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
   }
 
+  componentDidUpdate() {
+    console.log("component Did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
   render() {
-    const { name, location } = this.props;
-    const { count } = this.state;
-    console.log(this.props.name + " Children Render");
-
+    const { name, location, login, avatar_url } = this.state.userInfo;
     return (
       <div className="user-body">
-        <h2>Count = {count}</h2>
-        <button
-          onClick={() => {
-            this.setState({
-              count: count + 1,
-            });
-          }}
-        >
-          Count Increase
-        </button>
+        <img src={avatar_url} alt="logo" height={"200px"}/>
         <h2>{name}</h2>
         <h2>Location: {location}</h2>
-        <h2>Contact: @vinitchaudhari</h2>
-        {console.log(this.props.name + " Children Render 2")}
+        <h2>Login: {login}</h2>
       </div>
     );
   }
